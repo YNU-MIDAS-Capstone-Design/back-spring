26acaccbb0d119495c793f6fa59a90a6f7620de9
eunseo <es7837@naver.com>
2025-03-20 04:57:31 +0900

    edit folder

--- backend/src/main/java/backend/spring/dto/response/view/ViewHomeResponseDto.java
+++ backend/src/main/java/backend/spring/dto/response/view/ViewHomeResponseDto.java
@@ -1,24 +1,29 @@
-package backend.spring.dto.response;
+package backend.spring.dto.response.view;
 
 import java.util.List;
 
+import backend.spring.dto.response.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import backend.spring.common.ResponseCode;
 import backend.spring.common.ResponseMessage;
 import backend.spring.dto.object.ViewProjectData;
+import lombok.Getter;
 
-public class ViewProjectResponseDto extends ResponseDto {
-	List<ViewProjectData> projects;
+@Getter
+public class ViewHomeResponseDto extends ResponseDto {
+	private List<ViewProjectData> recent; //추천순
+	private List<ViewProjectData> popular; //인기순
 
-	private ViewProjectResponseDto(List<ViewProjectData> projects) {
+	private ViewHomeResponseDto(List<ViewProjectData> recent, List<ViewProjectData> popular){
 		super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
-		this.projects = projects;
+		this.recent = recent;
+		this.popular = popular;
 	}
 
-	public static ResponseEntity<ViewProjectResponseDto> success(List<ViewProjectData> projects){
-		ViewProjectResponseDto result = new ViewProjectResponseDto(projects);
+	public static ResponseEntity<ViewHomeResponseDto> success(List<ViewProjectData> recent, List<ViewProjectData> popular){
+		ViewHomeResponseDto result = new ViewHomeResponseDto(recent, popular);
 		return ResponseEntity.status(HttpStatus.OK).body(result);
 	}
 
@@ -26,4 +31,5 @@ public class ViewProjectResponseDto extends ResponseDto {
 		ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_PROJECT, ResponseMessage.NOT_EXISTED_PROJECT);
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
 	}
+
 }
