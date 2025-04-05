51530f58470be2584b9907b53e83fb00f5e70884
user <ynzxc123@gmail.com>
2025-04-05 18:55:25 +0900

    location 통일

--- backend/src/main/java/backend/spring/entity/enums/Location.java
+++ backend/src/main/java/backend/spring/entity/enums/Location.java
@@ -1,7 +1,6 @@
 package backend.spring.entity.enums;
 
 public enum Location {
-    서울특별시,
     경기도,
     강원도,
     충청북도,
@@ -9,10 +8,5 @@ public enum Location {
     전라북도,
     전라남도,
     경상북도,
-    경상남도,
-    부산,
-    대구,
-    대전,
-    세종,
-    제주도
+    경상남도
 }
