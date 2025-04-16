b41f6149f332e4a16dfaf7eadb94aa7bf51198b7
kes0226 <es7837@naver.com>
2025-04-16 17:33:51 +0900

    add cors config 3000

--- backend/src/main/java/backend/spring/config/WebConfig.java
+++ backend/src/main/java/backend/spring/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {  //백엔드의 CORS설정
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")  // 백엔드의 모든 api에 대해 cors 허용
-                        .allowedOrigins("http://localhost/3000")  //백엔드로 오는 요청을 허용할 브라우저의 origin(프로토콜, 호스트, 포트만 명시)
+                        .allowedOrigins("http://localhost:3000")  //백엔드로 오는 요청을 허용할 브라우저의 origin(프로토콜, 호스트, 포트만 명시)
                         .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
                         .allowedHeaders("*");  // 허용할 헤더
             }
