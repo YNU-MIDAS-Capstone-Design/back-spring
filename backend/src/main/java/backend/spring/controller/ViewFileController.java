f33b9e34dd80bff756c4b9092971a8e7476cc7e7
kes0226 <es7837@naver.com>
2025-05-04 20:41:29 +0900

    설명 주석 추가

--- backend/src/main/java/backend/spring/controller/ViewFileController.java
+++ backend/src/main/java/backend/spring/controller/ViewFileController.java
@@ -30,20 +30,24 @@ public class ViewFileController {
         if( fileName == null){
             return UploadFileResponseDto.databaseError(); //파일 저장 실패
         } else{
+            // fileName만 db에 저장하면 됨
+
             String image_url = "http://localhost:8080/api/view/profile_image/" + fileName;
+            //프론트에게 넘겨줄 때 이 url만 넘겨주면 됨. 그러면 그냥 프론트는 이 url에 img 태그 씌워서 사진 보여줌
+            //<img src="http://localhost:8080/api/view/profile_image/baf1e39d.jpg" alt="프로필 이미지" />
             return UploadFileResponseDto.success(image_url);
         }
     }
     @DeleteMapping("/delete/{file_name}")
     public ResponseEntity<ResponseDto> deleteImage(@PathVariable("file_name") String fileName){
         if(fileService.file_delete("profile_image", fileName)){
-            return ResponseDto.successResponse();
+            return ResponseDto.successResponse(); //파일 삭제 성공
         } else{
-            return ResponseDto.databaseError();
+            return ResponseDto.databaseError();  //파일 삭제 실패
         }
     }
 
-    @GetMapping("/profile_image/{file_name}")
+    @GetMapping("/profile_image/{file_name}")  //이미지를 보여주는 api
     public ResponseEntity<Resource> viewProfile(@PathVariable("file_name") String fileName) {
         try {
             String fileDir = "/app/data/profile_image/";
