743cd23ca9a273d21903718ef0b0a530b875dc43
user <ynzxc123@gmail.com>
2025-05-18 19:47:20 +0900

    fix 경로 수정

--- src/main/java/backend/spring/repository/ProjectApplicantRepository.java
+++ src/main/java/backend/spring/repository/ProjectApplicantRepository.java
@@ -1,3 +1,8 @@
+package backend.spring.repository;
+
+import backend.spring.entity.Project;
+import backend.spring.entity.ProjectApplicant;
+import backend.spring.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
